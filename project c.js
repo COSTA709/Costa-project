2fe86a920b1aead00a5b1204b4cc727ba4b22f60
COSTA709 <costacostanovic@gmail.com>
2024-09-20 03:31:25 +0200

    Add files via upload

---
+++ project c.js
@@ -0,0 +1,7 @@
+const h1 = document.createElement("h1");
+
+h1.textContent = "Copyrights©Costaplus/2024";
+
+h1.setAttribute("class", "text-xl text-black font-bold text-center my-8");
+
+document.body.append(h1);
