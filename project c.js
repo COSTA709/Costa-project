e986da687a5c6914d6e77126fec5a51ae6fd6961
COSTA709 <costacostanovic@gmail.com>
2026-09-11 23:42:12 +0200

    Delete project c.js

--- project c.js
+++
@@ -1,7 +0,0 @@
-const h1 = document.createElement("h1");
-
-h1.textContent = "Copyrights©Costaplus/2024";
-
-h1.setAttribute("class", "text-xl text-black font-bold text-center my-8");
-
-document.body.append(h1);
